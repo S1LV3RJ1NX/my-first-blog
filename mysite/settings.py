5611880ada9ed6a0641ab99d49f81c50839c16ef
s1lv3rj1nx <pratamesh1867@gmail.com>
2017-08-13 19:07:49 +0530

    changed allowed hosts in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x0()r_r*x0@xli9^otpr5-(rmno0f&wv^$0rvi+!e9zmgo#sbg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 's1lv3rj1nx.pythonanywhere.com']
 
 
 
